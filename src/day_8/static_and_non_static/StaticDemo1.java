6f6149b6a53f7989dba86872ea96e03dcac5edf2
Subhajoy Mukherjee <subhajoymukherjee2003@gmail.com>
2025-03-21 17:01:43 +0530

    Capgemini Java Codes Upto Day 8

--- src/day_8/static_and_non_static/StaticDemo1.java
+++ src/day_8/static_and_non_static/StaticDemo1.java
@@ -11,8 +11,9 @@ public class StaticDemo1 {
 	public static void main(String[] args)
 	{
 		StaticDemo1 d1=new StaticDemo1();
-		System.out.println(d1.name);
-		System.out.println(id);
-		System.out.println(StaticDemo1.id);
+		System.out.println(d1.name);	//name is a non static variable so to access it we have to declare a object of StaticDemo1 Class First
+		//System.out.println(d1.name);	//Error as we cannot access non static variable directly
+		System.out.println(id);			//No Error. As if we are in same class inside which we have declared the static variable then to access it we cannot have to use the class reference.
+		System.out.println(StaticDemo1.id);	//No Error. As inside the same class of the static variuavb
 	}
 }
